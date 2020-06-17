b51dc6121c663bc7ba78cff312441196156f913d
Kevin Huynh <kevinhuynh1@gmail.com>
2020-06-16 20:17:54 -0700

    latest

--- bin/dconf_all.sh
+++ bin/dconf_all.sh
@@ -5,17 +5,22 @@ if ! [ "$1" = "dump" ] || [ "$1" = "load" ]; then
   exit 1
 fi
 
-echo "Dumping WM Keybindings"
-CONF_FILE=configs/wm_keybindings.dconf
-DIR=/org/gnome/desktop/wm/keybindings/
-./bin/dconf.sh $1 $CONF_FILE $DIR
-
-echo "Dumping Gnome Terminal Settings"
+echo "$1 Gnome Terminal Settings"
 CONF_FILE=configs/gnome-terminal.dconf
 DIR=/org/gnome/terminal/
 ./bin/dconf.sh $1 $CONF_FILE $DIR
 
-echo "Dumping WM Custom Keybindings"
-CONF_FILE=configs/wm_custom_keybindings.dconf
+echo "$1 WM Keybindings"
+CONF_FILE=configs/keybindings_wm.dconf
+DIR=/org/gnome/desktop/wm/keybindings/
+./bin/dconf.sh $1 $CONF_FILE $DIR
+
+echo "$1 WM Custom Keybindings"
+CONF_FILE=configs/keybindings_wm_custom.dconf
 DIR=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/
+./bin/dconf.sh $1 $CONF_FILE $DIR
+
+echo "$1 Gnome Keybindings"
+CONF_FILE=configs/keybindings_gnome.dconf
+DIR=/org/gnome/shell/keybindings/
 ./bin/dconf.sh $1 $CONF_FILE $DIR
