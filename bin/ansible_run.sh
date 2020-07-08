512265ad9105842ec40e2336eb364a86884c1f67
Kevin Huynh <kevin@koala.local>
2020-07-07 22:00:48 -0700

    Adding OSX configuration

--- bin/ansible_run.sh
+++ bin/ansible_run.sh
@@ -3,5 +3,11 @@
 PLAYBOOK=${1:-configs.yml}
 HOST=127.0.0.1
 VAULT_PW_FILE=vault-pw
+COMMAND=ansible-playbook
 
-ansible-playbook --ask-become-pass --vault-id @$VAULT_PW_FILE --connection=local --inventory $HOST, $PLAYBOOK
+if [ "$(uname)" == "Darwin" ]; then
+  PREFIX=~/Library/Python/3.7/bin/
+  COMMAND="$PREFIX$COMMAND"
+fi
+
+$COMMAND --ask-become-pass --vault-id @$VAULT_PW_FILE --connection=local --inventory $HOST, $PLAYBOOK
--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -12,21 +12,42 @@ function printInstallSkip {
   echo "$1 is already installed...skipping."
 }
 
-sudo apt-get update
+UNAME="$(uname -s)"
 
-### ANSIBLE ###
-checkExists "ansible"
-if [ $? -eq 0 ]; then
-  printInstallSkip "Ansible"
+if [ "$UNAME" == "Linux" ]; then 
+
+  sudo apt-get update
+  
+  checkExists "ansible"
+  if [ $? -eq 0 ]; then
+    printInstallSkip "Ansible"
+  else
+    printInstall "Annsible"
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
+  
+    # This PPA doesn't have a release file for 20.02 Ubuntu
+    # which 'breaks' apt updates, so just remove it
+    sudo add-apt-repository --remove ppa:ansible/ansible
+  fi
+
+elif [ "$UNAME" == "Darwin" ]; then 
+
+  if [ ! -f /usr/local/bin/brew ]; then
+    echo "Installing Brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+
+  echo "Installing Ansible"
+  pip3 install --user ansible
+  export PATH=~/Library/Python/3.7/bin:$PATH
+
 else
-  printInstall "Annsible"
-  sudo apt-add-repository --yes --update ppa:ansible/ansible
-  sudo apt install ansible
 
-  # This PPA doesn't have a release file for 20.02 Ubuntu
-  # which 'breaks' apt updates, so just remove it
-  sudo add-apt-repository --remove ppa:ansible/ansible
-fi
+  echo "Can't run on this OS"
+  exit 1
+
+fi 
 
 mkdir -p galaxy
 ansible-galaxy install --roles-path galaxy -r requirements.yml
