408e0177a0b354d9f5da8bea9240ea910b956575
Kevin Huynh <kevinhuynh1@gmail.com>
2022-03-24 21:48:45 -0700

    Adding virtual instruments cronjob

--- scripts/sync_virtual_instruments.sh
+++ scripts/sync_virtual_instruments.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-set -e
+set -o errexit -o errtrace
 
 HOME=/home/kevin
 ARCHIVE_DIR=$HOME/Archive
+
+function error_notify {
+  XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send -u critical "Cron Task Failed" "Could not sync Virtual Instruments folder"
+}
+trap 'error_notify' ERR
 
 rsync -a --progress $ARCHIVE_DIR/VirtualInstruments $ARCHIVE_DIR/Data
