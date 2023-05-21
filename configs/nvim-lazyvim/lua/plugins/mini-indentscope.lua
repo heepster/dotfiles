4f692450dcc655be4ebb03c07684e4be20f24a2f
Kevin Huynh <kevinhuynh1@gmail.com>
2023-05-20 20:44:26 -0700

    Adding lazyvim

---
+++ configs/nvim-lazyvim/lua/plugins/mini-indentscope.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.indentscope",
+  opts = {
+    draw = {
+      animation = function(next, total_steps)
+        return 1
+      end,
+    },
+    -- symbol = "╎",
+  },
+}
