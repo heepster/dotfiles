5eb40bbbf738d0637bd05945f82ec2b9616e9171
Kevin Huynh <kevinh@ziprecruiter.com>
2023-10-24 11:16:53 -0700

    Latest

---
+++ configs/nvim-lazyvim/lua/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      -- {
+      --   "<leader>fp",
+      --   function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      --   desc = "Find Plugin File",
+      -- },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "vertical",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          i = {
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+            ["jk"] = "close",
+          },
+        },
+      },
+      setup = {
+        pickers = {
+          find_files = {},
+        },
+      },
+    },
+  },
+}
