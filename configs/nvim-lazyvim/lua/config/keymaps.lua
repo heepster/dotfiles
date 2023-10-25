13d5f39e569abc38731e0b6146b50f4b507db06a
Kevin Huynh <kevinh@ziprecruiter.com>
2023-10-25 08:59:19 -0700

    Latest

--- configs/nvim-lazyvim/lua/config/keymaps.lua
+++ configs/nvim-lazyvim/lua/config/keymaps.lua
@@ -2,12 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Remove keymaps for easy window traversal
--- (I prefer using <C-w>hjkl)
+-- Window traversals
 vim.keymap.del("n", "<C-h>")
 vim.keymap.del("n", "<C-j>")
 vim.keymap.del("n", "<C-k>")
 vim.keymap.del("n", "<C-l>")
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Focus window right" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Focus window left" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Focus window down" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Focus window up" })
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertical" })
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontal" })
 
 -- Easier Escape remaps
 vim.keymap.set({ "i" }, "jk", "<esc>", { desc = "Escape" })
@@ -25,6 +30,7 @@ vim.keymap.set({ "n", "i", "v" }, "<C-S>", "<esc>:wa<cr>", { desc = "Save all" }
 -- Functionality
 vim.keymap.set({ "n", "i", "v" }, "<C-e>", "<esc>:Neotree toggle<cr>", { desc = "Toggle files sidebar" })
 vim.keymap.set({ "n", "i", "v" }, "<C-p>", "<esc>:Telescope find_files<cr>", { desc = "Find files in cwd" })
+vim.keymap.set({ "n" }, "<leader>p", "<esc>:Telescope find_files<cr>", { desc = "Find files in cwd" })
 vim.keymap.set({ "n" }, "<C-f>", "<esc>:Telescope current_buffer_fuzzy_find<cr>", { desc = "Find in current file" })
 vim.keymap.set({ "n" }, "<C-F>", "<esc>:Telescope live_grep<cr>", { desc = "Find string in current directory" })
 vim.keymap.set({ "n" }, "<C-R>", "<esc>:Telescope resume<cr>", { desc = "Resume last telescope search" })
